42940f8743d019350d172e63aa9923fda05c2bf9
Pedro Gianoto <pedrogianoto584@gmail.com>
2023-11-07 22:13:17 -0400

    Exercises done for now

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+let str = "hey";
+let num = 10;
+let fullStr = "";
+const repeatString = function(str, num) {
+    if (num === 0) {
+        return '';
+      } else if (num < 0) {
+        return 'ERROR';
+      } else {
+        let repeatedStr = '';
+        for (let i = 0; i < num; i++) {
+          repeatedStr += str;
+        }
+        return repeatedStr;
+      }
+    };
+    const randomNum = Math.floor(Math.random() * 10) + 1;
 
-};
+// Call the repeatString function with the random number
+    const repeatedString = repeatString('hey', randomNum);
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,36 @@
-const reverseString = function() {
-
+let str = "";
+//Both methods below are built-in methods.
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
 };
+/*
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
+}
 
+
+//Below we have non-built-in method(s). 
+
+//The first one uses a for loop but it doesn't work for our test because it returns
+//the reverted string line by line and the code
+//reads only the first line it gets from the return.
+/*    const reverseString = function(str) {
+        let newString = "";
+        for (let i = str.length - 1; i >= 0; i--) {
+            newString += str[i];
+            return newString;
+        }
+    }
+*/
+//Using recursion
+/*const reverseString = function(str) {
+    if (str === "")
+    return "";
+    else   
+        return reverseString(str.substr(1)) + str.charAt(0);
+}
+*/
 // Do not edit below this line
-module.exports = reverseString;
